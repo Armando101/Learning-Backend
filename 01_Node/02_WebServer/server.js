b878bc55331c54907b71cc408be8b95aa4963c7d
Armano Rivera <rivera.armando997@gmail.com>
2021-01-04 01:35:41 -0600

    Node-WebServer; Usando parciales con handlebars

--- 01_Node/02_WebServer/server.js
+++ 01_Node/02_WebServer/server.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
 const port = 8080;
 
 app.use(express.static(__dirname + '/public'));
+
 // Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
   // Pasando valores de variables en la vista
   res.render('home', {
+    name: 'Armando',
+    year: new Date().getFullYear()
+  });
+})
+
+app.get('/about', (req, res) => {
+  // Pasando valores de variables en la vista
+  res.render('about', {
     name: 'Armando',
     year: new Date().getFullYear()
   });
