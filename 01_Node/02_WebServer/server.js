aaffbdb458aef55e18c78b934d7eaa98cd976bdd
Armano Rivera <rivera.armando997@gmail.com>
2021-01-04 00:48:05 -0600

    Node-WebServer; servido de contenido estático

--- 01_Node/02_WebServer/server.js
+++ 01_Node/02_WebServer/server.js
@@ -2,14 +2,16 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {
-  let salida = {
-    nombre: 'Armando',
-    age: 23,
-    url: req.url
-  };
-  res.send(salida);
-})
+app.use(express.static(__dirname + '/public'));
+
+// app.get('/', (req, res) => {
+//   let salida = {
+//     nombre: 'Armando',
+//     age: 23,
+//     url: req.url
+//   };
+//   res.send(salida);
+// })
 
 app.get('/data', (req, res)=> {
   res.send('Hello data');
